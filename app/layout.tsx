d80ed5779fbf40244af0f166310fe4dc3de859d7
ozair25 <sayedozair25@gmail.com>
2026-07-16 14:33:55 +0530

    ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,17 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable} ${alex.variable}`}>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LVNGNGWWY0" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LVNGNGWWY0');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://res.cloudinary.com" />
         <link rel="preconnect" href="https://images.unsplash.com" />
         <link rel="preconnect" href="https://picsum.photos" />
