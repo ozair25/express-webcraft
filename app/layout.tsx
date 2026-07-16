6791c67f146e93ed295b98f6945ffd5fdabcafb6
ozair25 <sayedozair25@gmail.com>
2026-07-16 15:11:19 +0530

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,17 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
             `,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xna4kj796k");
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://res.cloudinary.com" />
         <link rel="preconnect" href="https://images.unsplash.com" />
         <link rel="preconnect" href="https://picsum.photos" />
